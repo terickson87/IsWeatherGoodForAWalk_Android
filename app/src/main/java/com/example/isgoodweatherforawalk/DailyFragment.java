1dfeecd81d824c46e5a9dc6002ff8ad8d92e99e5
Todd Erickson <twericks@usc.edu>
2020-05-28 10:19:20 -0700

    Implemented much of the DailyFragment and HourlyFragment.

--- app/src/main/java/com/example/isgoodweatherforawalk/DailyFragment.java
+++ app/src/main/java/com/example/isgoodweatherforawalk/DailyFragment.java
@@ -1,12 +1,17 @@
 package com.example.isgoodweatherforawalk;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,50 +20,45 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class DailyFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MainActivity m_Activity;
+    private ArrayList<DailyWeatherData> m_DailyWeatherDataList;
 
     public DailyFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DailyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DailyFragment newInstance(String param1, String param2) {
+
+    public static DailyFragment newInstance() {
         DailyFragment fragment = new DailyFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+        m_Activity = (MainActivity) getActivity();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_daily, container, false);
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = fragmentView.findViewById(R.id.daily_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        m_DailyWeatherDataList = (ArrayList<DailyWeatherData>) m_Activity.getDailyWeatherData();
+
+
+
+        return  fragmentView;
     }
+
 }
